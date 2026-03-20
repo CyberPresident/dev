7447c059a8f110f18b9281e7a7fa24c09e006456
Jaylen Ellis <president@Jaylens-MacBook-Air.local>
2026-03-20 14:18:37 -0500

    remove unnecessary main.py

--- python-basics/main.py
+++
@@ -1,8 +0,0 @@
-import os
-import platform
-import socket
-
-print("Operating System:", platform.system())
-print("OS Version:", platform.version())
-print("Hostname:", socket.gethostname())
-print("Current Working Directory:", os.getcwd())
